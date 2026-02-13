89e131802a11ba4ff86bef6605b4e043ceec7aaf
Rexnice <ernestrex1996@gmail.com>
2026-02-13 02:39:17 +0100

    Configure for Vercel native Python runtime

--- app.py
+++ app.py
@@ -289,7 +289,9 @@ def delete_cv():
     return redirect(url_for('cv'))
 
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     with app.app_context():
+#         db.create_all()
+#     app.run(debug=True)
+
+application = app
